961e37a0ba61df261f9651f74d7fe84ec46905b8
Reynaldo <yer123451@hotmail.com>
2019-12-09 18:07:06 -0600

    Retomacion
    Recuperacion de archivos y retomando cursos

--- Java/UberBlack.java
+++ Java/UberBlack.java
@@ -1,11 +1,15 @@
-import java.java.util.Arraylist;
+import java.util.ArrayList;
 import java.util.Map;
 
 class UberBlack extends Car {
-    Map<String, ArrayList<String, Integer>> typeCarAccepted;
+    Map<String, ArrayList<String,Integer>> typeCarAccepted;
+    ArrayList<String> seatsMaterial;
 
-    public UberBlack(String license, Account driver, Map<String, ArrayList<String, Integer>> typeCarAccepte){
+    public UberBlack(String license, Account driver, 
+    Map<String, ArrayList<String,Integer>> typeCarAccepted,
+    ArrayList<String> seatsMaterial){
         super(license, driver);
+        this.typeCarAccepted = typeCarAccepted;
         this.seatsMaterial = seatsMaterial;
     }
 }
--- Java/main.java
+++ Java/main.java
@@ -1,13 +1,11 @@
 class Main{
     public static void main(String[] args){
         System.out.println("hola mundo");
-        Car car = new Car("AMQ123", new Account("Reynaldo PAredes", "AND38"));
-        car.passegenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Reynaldo PAredes", "AND38"), "Chevrolet", "Sonic" );
+        uberX.passegenger = 4;
+        uberX.printDataCar();
 
-        Car car2 = new Car("WEJD12", new Account("Erick Daniel", "abddb232"));
-        car2.passegenger= 2;
-        car2.printDataCar();
+        
         
     }
 }
